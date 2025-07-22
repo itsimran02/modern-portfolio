4a3af4e95e2ac7964f9df48f27bd72f5d032474a
imran <imranphynix56@gmail.com>
2025-07-23 00:55:32 +0530

    updated about

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -2,6 +2,7 @@
 
 import Spline from "@splinetool/react-spline";
 import SkillCards from "./ui/SkillCards";
+import Image from "next/image";
 
 const About = () => {
   const skillData1 = ["html", "css", "react", "js"];
@@ -70,6 +71,41 @@ const About = () => {
             <div>
               <SkillCards skillData={skillData2} />
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-7 md:grid md:grid-cols-3 grid-rows-2 w-full gap-4 h-[400px]">
+        <div
+          className=" flex-col col-span-1 row-span-1  rounded-xl flex overflow-clip items-center border relative border-[rgba(54,55,73,0.43)]"
+          style={{
+            background:
+              "linear-gradient(103deg, #04071D 16.66%, #0C0E23 81.61%)",
+          }}
+        >
+          <h3 className="text-2xl font-bold font-inter text-white pt-6 pl-6">
+            Tech enthusiast with a passion for development.
+          </h3>
+          <div className="self-end h-full">
+            <img
+              src="/about-labal.png"
+              alt="info"
+              className="w-full h-full"
+            />
+          </div>
+        </div>
+
+        <div className="bg-amber-400 col-span-2 row-span-2 rounded-xl flex items-center justify-center border border-[rgba(54,55,73,0.43)] ">
+          <div className="text-4xl font-bold text-black">
+            Hi! I span 2/3 width and full height
+          </div>
+        </div>
+
+        <div className="col-span-1 relative overflow-clip bg-[#06091F] row-span-1   p-6 rounded-xl flex items-center border border-[rgba(54,55,73,0.43)]">
+          <h3 className="text-2xl font-bold font-inter text-white">
+            Do you want to start a project together?
+          </h3>
+          <div className="absolute top-0 left-0">
+            <img src="/about-gradient.png" />
           </div>
         </div>
       </div>
